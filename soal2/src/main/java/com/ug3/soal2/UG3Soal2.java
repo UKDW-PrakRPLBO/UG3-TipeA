133b86f453e5467f9d330126128b24f290d928ea
Christina Andrea Putri <christinputri5@gmail.com>
2023-02-21 18:55:07 +0700

    del .idea

--- soal2/src/main/java/com/ug3/soal2/UG3Soal2.java
+++ soal2/src/main/java/com/ug3/soal2/UG3Soal2.java
@@ -4,10 +4,82 @@ package com.ug3.soal2;
  * Hello world!
  *
  */
-public class App 
+public class UG3Soal2
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws InterruptedException {
+        boolean red = true, yellow = false, green = false;
+        int countDown = 0;
+        trafficLight(red,yellow,green,countDown);
+    }
+
+    private static void trafficLight(
+            boolean red,
+            boolean yellow,
+            boolean green,
+            int countDown
+    ) throws InterruptedException {
+        if (red) {
+            System.out.println("Lampu merah menyala selama");
+            for(countDown = 20; countDown >0; countDown--){
+                System.out.println(countDown);
+                Thread.sleep(1000);
+            }
+            System.out.println("\nAkan berganti ke lampu kuning\n");
+            red = false;
+            yellow = true;
+            Thread.sleep(2000);
+            yellowTurnOn(red,yellow,green,countDown);
+        }
+    }
+
+    private static void yellowTurnOn(
+            boolean red,
+            boolean yellow,
+            boolean green,
+            int countDown
+    ) throws InterruptedException {
+        if(yellow){
+            System.out.println("Lampu kuning menyala selama");
+            for(countDown = 2;countDown >0; countDown--){
+                System.out.println(countDown);
+                Thread.sleep(1000);
+            }
+            System.out.println("\nAkan berganti ke lampu hijau\n");
+            yellow = false;
+            green = true;
+            Thread.sleep(1000);
+            greenTurnedOn(red,yellow,green,countDown);
+        }
+    }
+
+    private static void greenTurnedOn(
+            boolean red,
+            boolean yellow,
+            boolean green,
+            int countDown
+    ) throws InterruptedException {
+        if(green){
+            System.out.println("Lampu hijau menyala selama");
+            for(countDown = 15; countDown > 0; countDown--){
+                System.out.println(countDown);
+                Thread.sleep(1000);
+            }
+            System.out.println("\nAkan berganti ke lampu kuning");
+            green = false;
+            yellow = true;
+            Thread.sleep(1000);
+            if(yellow){
+                System.out.println("Lampu kuning menyala selama");
+                for(countDown = 2; countDown > 0; countDown--){
+                    System.out.println(countDown);
+                    Thread.sleep(1000);
+                }
+                System.out.println("\nAkan berganti ke lampu merah\n");
+                yellow = false;
+                red = true;
+                Thread.sleep(1000);
+                trafficLight(red,yellow,green,countDown);
+            }
+        }
     }
 }
--- soal2/src/test/java/com/ug3/soal2/UG3Soal2Test.java
+++ soal2/src/test/java/com/ug3/soal2/UG3Soal2Test.java
@@ -1,14 +1,47 @@
 package com.ug3.soal2;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
+ *
  * Unit test for simple App.
  */
 public class UG3Soal2Test
-
 {
 
+    static UG3Soal2 check = new UG3Soal2();
+    private static ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeAll
+    public static void init() {
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        //init stream captors fo every test case. so that not interfere with prev test parameterized test
+        outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        //give back the System out to default. (actually not needed)
+        System.setOut(System.out);
+    }
+
+
+    
 }
