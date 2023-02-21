d99bbb7e0f77dbb8a52f956ef6e6903b5a2791ce
Christina Andrea Putri <christinputri5@gmail.com>
2023-02-21 13:26:16 +0700

    add changes to pom.xml

--- soal1/src/main/java/com/ug3/soal1/UGSoal1.java
+++ soal1/src/main/java/com/ug3/soal1/UGSoal1.java
@@ -1,11 +1,15 @@
 package com.ug3.soal1;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class UGSoal1
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World");
-   
+        System.out.println("Welcome to UG3!");
     }
+
+
 }
