b0ed48a24959f4eb01722bf61225c941adf7d74a
Christina Andrea Putri <christinputri5@gmail.com>
2023-02-21 17:27:20 +0700

    add changes for test case

--- soal1/src/test/java/com/ug3/soal1/UGSoal1Test.java
+++ soal1/src/test/java/com/ug3/soal1/UGSoal1Test.java
@@ -1,8 +1,6 @@
 package com.ug3.soal1;
 
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.junit.Before;
+
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -16,69 +14,59 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.*;
-
-import static com.ug3.soal1.UGSoal1.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
  * Unit test for simple App.
  */
-public class UGSoal1Test
+public class UGSoal1Test {
+    UGSoal1 absen = new UGSoal1();
 
-{
-    static UGSoal1 absen = new UGSoal1();
+    private ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-    private final  InputStream systemIn = System.in;
-    private final PrintStream standardOut = System.out;
-    private static final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    @BeforeAll
+    public static void init() {
 
-
+    }
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
+        //init stream captors fo every test case. so that not interfere with prev test parameterized test
+        outputStreamCaptor = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @AfterEach
-    public void tearDown(){
-        System.setIn(systemIn);
-        System.setOut(standardOut);
+    public void tearDown() {
+        //give back the System out to default. (actually not needed)
+        System.setOut(System.out);
     }
 
-
-    private static Stream<Arguments> forAbsenTest(){
-
-    Date thisDate = new Date();
-    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMMM-yy");
-    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
-    String date= dateFormat.format(thisDate);
-    String time = timeFormat.format(thisDate);
-    String name = "Nana";
-        return Stream.of(
-                Arguments.of(name,date,time,"berhasil absensi\nAtas nama : " +
-                        name + "\n" + "Pada tanggal : " + date + "\n" +
-                        "Pukul : " + time)
-        );
-}
-    @ParameterizedTest
-    @MethodSource("forAbsenTest")
-    public void absenTest(String name, String date, String time, String expected){
+    private static Stream<Arguments> forAbsenTest() {
         Date thisDate = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMMM-yy");
         SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
-        date= dateFormat.format(thisDate);
-        time = timeFormat.format(thisDate);
-        String input = "Nana";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        PrintStream printStream = new PrintStream(outputStreamCaptor);
-        System.setOut(printStream);
-        absen.absen(name,date,time);
-        assertEquals( expected, outputStreamCaptor.toString().trim());
-
-
+        String date = dateFormat.format(thisDate);
+        String time = timeFormat.format(thisDate);
+        return Stream.of(
+                Arguments.of("Nana", date, time, "berhasil absensi\nAtas nama : " +
+                        "Nana" + "\n" + "Pada tanggal : " + date + "\n" +
+                        "Pukul : " + time),
+                Arguments.of("Dodi", date, time, "berhasil absensi\nAtas nama : " +
+                        "Dodi" + "\n" + "Pada tanggal : " + date + "\n" +
+                        "Pukul : " + time),
+                Arguments.of("Anton", date, time, "berhasil absensi\nAtas nama : " +
+                        "Anton" + "\n" + "Pada tanggal : " + date + "\n" +
+                        "Pukul : " + time)
+        );
     }
 
+    @ParameterizedTest
+    @MethodSource("forAbsenTest")
+    public void absenTest(String name, String date, String time, String expected) {
+        absen.absen(name, date, time);
+        assertEquals(expected, outputStreamCaptor.toString().trim());
+    }
 
 }
