1fa4f7fae983ac12dfa36c9598313603e00868e2
Christina Andrea Putri <christinputri5@gmail.com>
2023-02-21 18:51:19 +0700

    delete target and idea

---
+++ soal2/src/test/java/com/ug3/soal2/UG3Soal2Test.java
@@ -0,0 +1,14 @@
+package com.ug3.soal2;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class UG3Soal2Test
+
+{
+
+}
