faabf15cf3627945a66496bd8de8842d9bdf6fba
Christina Andrea Putri <christinputri5@gmail.com>
2023-02-22 01:31:00 +0700

    fix soal 2

--- soal2/src/test/java/com/ug3/soal2/UG3Soal2Test.java
+++ soal2/src/test/java/com/ug3/soal2/UG3Soal2Test.java
@@ -1,47 +1,52 @@
 package com.ug3.soal2;
 
+
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
- *
  * Unit test for simple App.
  */
 public class UG3Soal2Test
 {
 
-    static UG3Soal2 check = new UG3Soal2();
-    private static ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    UG3Soal2 testFib = new UG3Soal2();
 
     @BeforeAll
     public static void init() {
 
     }
-
     @BeforeEach
     public void setUp() {
-        //init stream captors fo every test case. so that not interfere with prev test parameterized test
+
         outputStreamCaptor = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @AfterEach
     public void tearDown() {
-        //give back the System out to default. (actually not needed)
         System.setOut(System.out);
     }
 
+    private static Stream<Arguments> forFibonacciTest() {
+        return Stream.of(
+                Arguments.of("Halo halo bandung", "Bandung kota favoritku", "Bilangan Fibonacci\n" + "17 22 39 61 100 161 261 422 683 1105 1788")
+        );
+    };
 
-    
+    @ParameterizedTest
+    @MethodSource("forFibonacciTest")
+    public void fibonacciTest(String first, String second,String expected){
+        testFib.fibonacci(first,second);
+
+        assertEquals(expected,outputStreamCaptor.toString().trim());
+    }
 }
