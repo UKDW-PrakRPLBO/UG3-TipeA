f3a3c3988b0b7484476b063db0b7dbd97155adeb
Christina Andrea Putri <christinputri5@gmail.com>
2023-02-22 15:57:51 +0700

    add parameter

--- soal2/src/test/java/com/ug3/soal2/UG3Soal2Test.java
+++ soal2/src/test/java/com/ug3/soal2/UG3Soal2Test.java
@@ -38,7 +38,11 @@ public class UG3Soal2Test
 
     private static Stream<Arguments> forFibonacciTest() {
         return Stream.of(
-                Arguments.of("Halo halo bandung", "Bandung kota favoritku", "Bilangan Fibonacci\n" + "17 22 39 61 100 161 261 422 683 1105 1788")
+                Arguments.of("Halo halo bandung", "Bandung kota favoritku", "Bilangan Fibonacci\n" + "17 22 39 61 100 161 261 422 683 1105 1788"),
+                Arguments.of("Matcha latte itu enak","Kopi susu is better","Bilangan Fibonacci\n" + "21 19 40 59 99 158 257 415 672 1087 1759"),
+                Arguments.of("jalan jalan ke Bali","Bali adalah tempat favoritku","Bilangan Fibonacci\n" + "19 28 47 75 122 197 319 516 835 1351 2186"),
+                Arguments.of("I miss you","ich liebe dich","Bilangan Fibonacci\n" + "10 14 24 38 62 100 162 262 424 686 1110"),
+                Arguments.of("do re mi fa","sol la si do","Bilangan Fibonacci\n" + "11 12 23 35 58 93 151 244 395 639 1034")
         );
     };
 
